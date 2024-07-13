a80b901e8188de99123940d54cb1bb44b5446ee9
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-13 13:11:18 +0200

    [UPD]  cretaed header and footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
+@section('main')
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                Sono La parte Main
+            </div>
+        </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    <h1>
-        Prova
-    </h1>
-
-    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
-
-</body>
-
-</html>
+    </div>
+@endsection
